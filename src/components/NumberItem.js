98d4d5984885998675048b3be476fde201e76d8d
nigranac <can.argin.16@gmail.com>
2020-11-10 02:21:35 +0300

    addNumber

--- src/components/NumberItem.js
+++ src/components/NumberItem.js
@@ -1,32 +1,24 @@
 import React from 'react';
-import {
-  Text,
-  Pressable,
-} from 'react-native';
+import {Text, Pressable} from 'react-native';
 
 import {numberItem} from '../styles';
 
 const NumberItem = (props) => {
-
-
-
   return (
     <Pressable
       onPress={() => props.onChange()}
-      style={({}) => [
+      style={({pressed}) => [
         numberItem.container,
-        {backgroundColor: props.item.isCheck ? '#2196f3': 'black'},
+        {backgroundColor: props.item.isCheck ? '#2196f3' : 'black'},
       ]}>
-
-    {({pressed}) => (
-
-      <Text 
-        style={[
-          numberItem.text,{color: props.item.isCheck ? 'black' : 'white'}
-          
-        ]}>
-        {props.item.number}
-      </Text>
+      {({pressed}) => (
+        <Text
+          style={[
+            numberItem.text,
+            {color: props.item.isCheck ? 'black' : 'white'},
+          ]}>
+          {props.item.number}
+        </Text>
       )}
     </Pressable>
   );
