5339ca5e2c99ab5fc766655efb3d0b7385d3b282
nigranac <can.argin.16@gmail.com>
2020-11-10 02:20:01 +0300

    addFunction

--- src/pages/EnterPoem.js
+++ src/pages/EnterPoem.js
@@ -1,43 +1,40 @@
-import React, { useState} from 'react';
-import {SafeAreaView, View, Text, ScrollView,FlatList} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, View, Text, ScrollView, FlatList} from 'react-native';
 import {NumberItem, Button} from '../components';
 import {numberItem, enterPoem} from '../styles';
 
 const numbers = [
-  {id:0,number: 1, isCheck: false},
-  {id:1,number: 5, isCheck: false},
-  {id:2,number: 10, isCheck: false},
-  {id:3,number: 15, isCheck: false},
+  {id: 0, number: 1, isCheck: false, option: true},
+  {id: 1, number: 5, isCheck: false, option: false},
+  {id: 2, number: 10, isCheck: false, option: false},
+  {id: 3, number: 15, isCheck: false, option: false},
 ];
-
-
 
 const EnterPoem = (props) => {
   const [list, setList] = useState(numbers);
-  
+
+
   function selectPage() {
     props.navigation.navigate('Period');
   }
 
- async function changeColor(nums) {
-    const newList =list
-    const numberIndex = newList.findIndex(item=>item.id==nums)
-    newList[numberIndex].isCheck = !newList[numberIndex].isCheck
-    setList(newList)
+  async function changeColor(nums) {
+
+    let reset = [
+      { id: 0, number: 1, isCheck: false },
+      { id: 1, number: 5, isCheck: false },
+      { id: 2, number: 10, isCheck: false },
+      { id: 3, number: 15, isCheck: false },
+    ]
+    const numberIndex = reset.findIndex(item => item.id == nums)
+    reset[numberIndex].isCheck = true;
+    setList(reset)
+
   }
 
-
-
-
-const renderNumber= ({item})=>{
-  return(
-    <NumberItem
-     
-     item={item}
-     onChange={() => changeColor(item.id)}
-     />
-  )
-}
+  const renderNumber = ({item}) => {
+    return <NumberItem item={item} onChange={() => changeColor(item.id)} />;
+  };
 
   return (
     <SafeAreaView style={{flex: 1}}>
@@ -50,10 +47,10 @@ const renderNumber= ({item})=>{
 
         <View style={numberItem.viewNumbers}>
           <FlatList
-          data={list}
-          renderItem={renderNumber}
-          keyExtractor={(item,index)=>index.toString()}
-          numColumns={2}
+            data={list}
+            renderItem={renderNumber}
+            keyExtractor={(item, index) => index.toString()}
+            numColumns={2}
           />
         </View>
 
@@ -66,5 +63,12 @@ const renderNumber= ({item})=>{
 };
 
 export {EnterPoem};
-
-
+    // const newList = list;
+    // let newNumbers = []
+    // for (let box in numbers){
+    //    newNumbers.push({...box, isCheck: false});
+    // }
+    // setList(newNumbers);
+    // const numberIndex = newList.findIndex((item) => item.id == nums);
+    // newList[numberIndex].isCheck = !newList[numberIndex].isCheck;
+    // setList(newList);
