cdd1b328699edc08a40c5ad2468fcc84f27840c1
nigranac <can.argin.16@gmail.com>
2020-11-09 16:36:31 +0300

    numbItem

--- src/components/NumberItem.js
+++ src/components/NumberItem.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import {
-  SafeAreaView,
-  View,
   Text,
-  TouchableOpacity,
-  FlatList,
-  TouchableHighlight
+  Pressable,
 } from 'react-native';
 
 import {numberItem} from '../styles';
 
 const NumberItem = (props) => {
-  // console.log(props.item.isCheck)
+
+
+
   return (
-    <TouchableHighlight
-    onPress={()=>props.onChange()}
-    style={[numberItem.container,{backgroundColor:props.item.isCheck ? "blue":"black"}]}>
-      <Text style={numberItem.text}>{props.item.number}</Text>
-    </TouchableHighlight>
+    <Pressable
+      onPress={() => props.onChange()}
+      style={({}) => [
+        numberItem.container,
+        {backgroundColor: props.item.isCheck ? '#2196f3': 'black'},
+      ]}>
+
+    {({pressed}) => (
+
+      <Text 
+        style={[
+          numberItem.text,{color: props.item.isCheck ? 'black' : 'white'}
+          
+        ]}>
+        {props.item.number}
+      </Text>
+      )}
+    </Pressable>
   );
 };
 
 export {NumberItem};
-// {backgroundColor:props.item.isCheck?"#2196f3":"black"}
+
