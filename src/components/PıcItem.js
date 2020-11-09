72b01140a77673b1d7213ca8a28fd5a0f8d617bc
nigranac <can.argin.16@gmail.com>
2020-11-09 16:36:08 +0300

    picItem

--- src/components/PıcItem.js
+++ src/components/PıcItem.js
@@ -8,24 +8,39 @@ import {
   Image,
   ImageBackground,
   Dimensions,
-  TouchableHighlight
+  TouchableHighlight,
+  Pressable,
 } from 'react-native';
-
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {numberItem, picItem} from '../styles';
 
 const PıcItem = (props) => {
-  console.log(props.item.img);
+  console.log(props.item.isCheck);
   return (
-    <TouchableHighlight style={picItem.general}>
-      <ImageBackground imageStyle={{ borderRadius: 20 }}
-                       style={picItem.container}
-                       source={props.item.img}>
-        <Text style={picItem.text}>{props.item.name}</Text>
-        <Text style={picItem.text}>{props.item.period}</Text>
-      </ImageBackground>
-    </TouchableHighlight>
-
-
+    <Pressable onPress={() => props.onChange()} style={picItem.general}>
+      {({pressed}) => (
+        <ImageBackground
+          imageStyle={{borderRadius: 20}}
+          style={picItem.container}
+          source={props.item.img}>
+          <Pressable
+            // onPress={() => props.onChange()}
+            style={({}) => [
+              picItem.pressableStyle,
+              {backgroundColor: props.item.isCheck ? 'white' : null},
+            ]}>
+            <Icon
+              style={{display: props.item.isCheck ? 'flex' : 'none'}}
+              name="check"
+              size={30}
+              color="black"
+            />
+          </Pressable>
+          <Text style={picItem.text}>{props.item.name}</Text>
+          <Text style={picItem.text}>{props.item.period}</Text>
+        </ImageBackground>
+      )}
+    </Pressable>
   );
 };
 
